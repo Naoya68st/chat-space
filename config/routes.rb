09c4d9b26b54d3d6e85ecd0553c0fe9acf8336f3
Naoya68st <n_shimbo@outlook.com>
2020-03-31 00:31:54 +0900

    group_message_display

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "groups#index" #ルートパスへのアクセスがあったら、messages_controllerのindexのアクションが呼び出される
+  root to: "groups#index" #ルートパスへのアクセスがあったら、messages_controllerのindexのアクションが呼び出される
   resources :users, only: [:edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
